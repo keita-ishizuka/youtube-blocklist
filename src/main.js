42e91171c9704824fb5e802011cb7ceafcb8962a
keita_ishizuka <stone.atiek@gmail.com>
2022-12-29 10:23:55 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         youtube-blocklist
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description
+// @author       zukash, keita-ishizuka
+// @match        https://www.youtube.com/*
+// @icon
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // Your code here...
+})();
